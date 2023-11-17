f65023c9b82e7ac75d105049bebf6ff3aad71d43
ArsalanAhmedKhatyan2000 <arsal.ik2008@gmail.com>
2023-11-17 17:58:55 +0500

    Increase Cart Item Quantity By One API Created

--- routes/cart_router.js
+++ routes/cart_router.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const route = express.Router();
 const { verifyToken, verifyTokenAndAdminAuthorization } = require('../middleware/token_verifier');
-const { addToCart, removeItemFromCart, getCartProducts, clearCart, getCartProductsIDs } = require('../controllers/cart_controller');
+const { addToCart, removeItemFromCart, getCartProducts, clearCart, getCartProductsIDs, increaseCartItemQuantityByOne, decreaseCartItemQuantityByOne } = require('../controllers/cart_controller');
 
 route.put('/', verifyToken, addToCart);
 route.get('/getCart', verifyToken, getCartProducts);
 route.get('/getCartProductsIDs', verifyToken, getCartProductsIDs);
 route.delete('/removeItemFromCart', verifyToken, removeItemFromCart);
 route.delete('/clearCart', verifyToken, clearCart);
+route.put('/increaseCartItemQuantity', verifyToken, increaseCartItemQuantityByOne);
+route.put('/decreaseCartItemQuantityByOne', verifyToken, decreaseCartItemQuantityByOne);
 
 
 module.exports = route;
